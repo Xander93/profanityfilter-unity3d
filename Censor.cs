43050e96703a60f5320b51280345077758eb387a
Xander <xdg1993@gmail.com>
2016-11-02 16:48:27 +0100

    unity3d-profanityfilter
    + censor.cs

---
+++ Censor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+using System.Text.RegularExpressions;
+
+public class Censor : MonoBehaviour
+{
+    private string input = "foo word bar word2 foobar another akakkaak cyka blyat";
+    private string pattern = @"\b(?:word|cyka|word2|another)\b";
+    private string result;
+
+    void Start() {
+        result = Regex.Replace(input, pattern, m => new String('*', m.Length));
+        print(result);
+    }
+}
